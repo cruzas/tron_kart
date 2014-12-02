a6437b92ec0f93330b3c8a1ed00befe14ce1b53b
cruzas <samuel_cruz@outlook.com>
2014-12-02 15:38:34 +0100

    Test file.

---
+++ Hello.py
@@ -0,0 +1,18 @@
+import cocos
+
+class window(cocos.layer.Layer):
+    def __init__(self):
+        super(window, self).__init__()
+
+        label = cocos.text.Label('Hello, world', font_name='Times New Roman',
+                                 font_size=32, anchor_x='center',
+                                 anchor_y='center')
+        self.add(label)
+        label.position = 320, 240
+        
+cocos.director.director.init()
+
+win = window()
+
+main_scene = cocos.scene.Scene(win)
+cocos.director.director.run(main_scene)
