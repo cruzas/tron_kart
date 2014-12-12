9b3048bb198561e1e73f621131c9ea760975f504
Samuel Cruz <samuel_cruz@outlook.com>
2014-12-12 22:03:48 +0100

    New colours added

--- main/colors.py
+++ main/colors.py
@@ -5,12 +5,15 @@ Define more colors, if you need!
 # USUAL COLORS
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+CYAN = (0, 255, 255)
+GREEN = (0, 255, 0)
+GREY = (229, 229, 229)
+PINK = (255, 0, 255)
 RED = (255, 0, 0)
-GREEN = (0, 155, 0)
-BLUE = (111, 195, 223)
 YELLOW = (255, 255, 0)
-CYAN = (111, 195, 223)
+# GREEN = (0, 155, 0)
+# BLUE = (111, 195, 223)
+# CYAN = (111, 195, 223)
 
-# COLORS USED BY TAILS
-TRON_Y = (245, 245, 10)
-TRON_O = (245, 135, 10)
+
