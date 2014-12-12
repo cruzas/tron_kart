05b1a5f58e2dacdc672a89a6e60a6443358a8ff9
nelson <dossan@usi.ch>
2014-12-12 19:08:33 +0100

    Committing suicide :D

---
+++ studying/examples/doc_for_functions.py
@@ -0,0 +1,11 @@
+"""
+This function shows you that you can write documentation for a function,
+and get it calling its property __doc__, like in the example!
+"""
+
+def func():
+    """ THIS ARE MY DOCS """
+    print("STUPID FUNCTION")
+#
+
+print(func.__doc__)
