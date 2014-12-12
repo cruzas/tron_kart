4aafa9f815105dcb38d579dd255d05f10497ba42
nelson <dossan@usi.ch>
2014-12-12 19:04:18 +0100

    changed usual colors of this daily routine

--- main/colors.py
+++ main/colors.py
@@ -2,6 +2,7 @@
 Define more colors, if you need!
 '''
 
+# USUAL COLORS
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
@@ -9,5 +10,7 @@ GREEN = (0, 155, 0)
 BLUE = (111, 195, 223)
 YELLOW = (255, 255, 0)
 CYAN = (111, 195, 223)
+
+# COLORS USED BY TAILS
 TRON_Y = (245, 245, 10)
 TRON_O = (245, 135, 10)
