dad9da10f02027583cabbc73a374ff654b4c1a57
Samuel Cruz <samuel_cruz@outlook.com>
2014-12-14 17:25:18 +0100

    Slight correction in comment.

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ Run this script to watch the application.
 from src.trongrid import *
 
 
-"""Why am using if __name__ == '__main__': ?
+"""Why am I using if __name__ == '__main__': ?
 http://stackoverflow.com/questions/419163/what-does-if-name-main-do
 """
 
